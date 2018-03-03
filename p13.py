2ed4cae73de186b88833a9d3117cab4f30885f9d
Sunjay Dhama <sdsunjay73@yahoo.com>
2018-03-02 23:45:31 -0800

    added Naive Bayes classifier training and testing, as well as accuracy output

--- p13.py
+++ p13.py
@@ -15,20 +15,26 @@ documents = [(list(movie_reviews.words(fileid)), category) for category in movie
 random.shuffle(documents)
 
 all_words = []
+
 for w in movie_reviews.words():
     all_words.append(w.lower())
 
 all_words = nltk.FreqDist(all_words)
 
-# use top 3000 words
-word_features = list(all_words.keys())[:3000]
-
 def find_features(document):
     words = set(document)
+    # use top 3000 words
+    word_features = list(all_words.keys())[:3000]
     features = {}
-    for w in word_feaures:
-        features[w] = {w in words}
+    for w in word_features:
+        features[w] = (w in words)
     return features
-print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
 
+# print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+train_set, test_set = featuresets[1900:], featuresets[1900:]
+
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print("Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, test_set))*100)
+classifier.show_most_informative_features(15)
