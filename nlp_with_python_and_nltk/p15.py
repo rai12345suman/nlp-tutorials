3d92c9f8b0ed0c44ea0ceeae8e059de68779eb98
Sunjay Dhama <sdsunjay73@yahoo.com>
2018-03-08 23:02:17 -0800

    Reshuffled code, added some params to classifiers, made more robust

--- nlp_with_python_and_nltk/p15.py
+++ nlp_with_python_and_nltk/p15.py
@@ -14,28 +14,16 @@ import nltk
 import random
 from nltk.corpus import movie_reviews
 from nltk.classify.scikitlearn import SklearnClassifier
-import pickle
+import string
 
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 
-# list of tuples
-documents = [(list(movie_reviews.words(fileid)), category)
-             for category in movie_reviews.categories() for fileid in movie_reviews.fileids(category)]
 
-random.shuffle(documents)
-
-all_words = []
-
-for w in movie_reviews.words():
-    all_words.append(w.lower())
-
-all_words = nltk.FreqDist(all_words)
-
-
-def find_features(document):
+def find_features(document, all_words):
     words = set(document)
+
     # use top 3000 words
     word_features = list(all_words.keys())[:3000]
     features = {}
@@ -43,51 +31,93 @@ def find_features(document):
         features[w] = (w in words)
     return features
 
-featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
-train_set, test_set = featuresets[1900:], featuresets[1900:]
+def remove_punctuation_from_string(s):
+    translator = str.maketrans('', '', string.punctuation)
+    return s.translate(translator)
 
-classifier = nltk.NaiveBayesClassifier.train(train_set)
 
-print("Classic Naive Bayes Classifier accuracy percent:",
-      (nltk.classify.accuracy(classifier, test_set)) * 100)
+def remove_punctuation_from_list(all_words):
+    all_words = [''.join(c for c in s if c not in string.punctuation)
+                 for s in all_words]
+    # Remove the empty strings:
+    all_words = [s for s in all_words if s]
+    return all_words
 
-MNB_classifier = SklearnClassifier(MultinomialNB())
-MNB_classifier.train(train_set)
-print("Multinomial Naive Bayes Classifier accuracy percent:",
-      (nltk.classify.accuracy(MNB_classifier, test_set)) * 100)
 
-print("Skipping Gaussian Bayes Classifier accuracy percent")
-# GNB_classifier = SklearnClassifier(GaussianNB())
-# GNB_classifier.train(train_set)
-# print("Gaussian Naive Bayes Classifier accuracy percent:", (nltk.classify.accuracy(GNB_classifier, test_set))*100)
+def train_and_test_classifiers(train_set, test_set):
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
+    print("Classic Naive Bayes Classifier accuracy percent:",
+          (nltk.classify.accuracy(classifier, test_set)) * 100)
 
-BNB_classifier = SklearnClassifier(BernoulliNB())
-BNB_classifier.train(train_set)
-print("Bernoulli Naive Bayes Classifier accuracy percent:",
-      (nltk.classify.accuracy(BNB_classifier, test_set)) * 100)
+    MNB_classifier = SklearnClassifier(MultinomialNB())
+    MNB_classifier.train(train_set)
+    print("Multinomial Naive Bayes Classifier accuracy percent:",
+          (nltk.classify.accuracy(MNB_classifier, test_set)) * 100)
 
-LG_classifier = SklearnClassifier(LogisticRegression())
-LG_classifier.train(train_set)
-print("Logistic Regression Classifier accuracy percent:",
-      (nltk.classify.accuracy(LG_classifier, test_set)) * 100)
+    print("Skipping Gaussian Bayes Classifier accuracy percent")
+    # GNB_classifier = SklearnClassifier(GaussianNB())
+    # GNB_classifier.train(train_set)
+    # print("Gaussian Naive Bayes Classifier accuracy percent:", (nltk.classify.accuracy(GNB_classifier, test_set))*100)
 
-SGD_classifier = SklearnClassifier(SGDClassifier())
-SGD_classifier.train(train_set)
-print("Stochastic Gradient Descent Classifier accuracy percent:",
-      (nltk.classify.accuracy(SGD_classifier, test_set)) * 100)
+    BNB_classifier = SklearnClassifier(BernoulliNB())
+    BNB_classifier.train(train_set)
+    print("Bernoulli Naive Bayes Classifier accuracy percent:",
+          (nltk.classify.accuracy(BNB_classifier, test_set)) * 100)
 
-SVC_classifier = SklearnClassifier(SVC())
-SVC_classifier.train(train_set)
-print("C-Support Vector Classifier accuracy percent:",
-      (nltk.classify.accuracy(SVC_classifier, test_set)) * 100)
+    LG_classifier = SklearnClassifier(LogisticRegression())
+    LG_classifier.train(train_set)
+    print("Logistic Regression Classifier accuracy percent:",
+          (nltk.classify.accuracy(LG_classifier, test_set)) * 100)
 
-LinearSVC_classifier = SklearnClassifier(LinearSVC())
-LinearSVC_classifier.train(train_set)
-print("Linear Support Vector Classifier accuracy percent:",
-      (nltk.classify.accuracy(LinearSVC_classifier, test_set)) * 100)
+    SGD_classifier = SklearnClassifier(SGDClassifier())
+    SGD_classifier.train(train_set)
+    print("Stochastic Gradient Descent Classifier accuracy percent:",
+          (nltk.classify.accuracy(SGD_classifier, test_set)) * 100)
 
-NuSVC_classifier = SklearnClassifier(NuSVC())
-NuSVC_classifier.train(train_set)
-print("Nu-Support Vector Classifier accuracy percent:",
-      (nltk.classify.accuracy(NuSVC_classifier, test_set)) * 100)
+    SVC_classifier = SklearnClassifier(SVC())
+    SVC_classifier.train(train_set)
+    print("C-Support Vector Classifier accuracy percent:",
+          (nltk.classify.accuracy(SVC_classifier, test_set)) * 100)
+
+    LinearSVC_classifier = SklearnClassifier(LinearSVC())
+    LinearSVC_classifier.train(train_set)
+    print("Linear Support Vector Classifier accuracy percent:",
+          (nltk.classify.accuracy(LinearSVC_classifier, test_set)) * 100)
+
+    NuSVC_classifier = SklearnClassifier(NuSVC())
+    NuSVC_classifier.train(train_set)
+    print("Nu-Support Vector Classifier accuracy percent:",
+          (nltk.classify.accuracy(NuSVC_classifier, test_set)) * 100)
+
+# Main
+def main():
+
+    # list of tuples
+    documents = [(list(movie_reviews.words(fileid)), category)
+                 for category in movie_reviews.categories() for fileid in movie_reviews.fileids(category)]
+
+    # Everyday I'm shufflin'
+    random.shuffle(documents)
+
+    all_words = []
+    
+    # add all words in the movie reviews to list
+    for w in movie_reviews.words():
+        all_words.append(w.lower())
+
+    all_words = remove_punctuation_from_list(all_words)
+    all_words = nltk.FreqDist(all_words)
+
+    featuresets = [(find_features(rev, all_words), category)
+                   for (rev, category) in documents]
+
+    # split into training and testing
+    train_set = featuresets[:1900]
+    test_set = featuresets[1900:]
+
+    # new code
+    train_and_test_classifiers(train_set, test_set)
+
+if __name__ == '__main__':
+    main()
