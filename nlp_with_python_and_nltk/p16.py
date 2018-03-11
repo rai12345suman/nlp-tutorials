3ad2bf7c44f23e9076b6556981ea36ad82136718
Sunjay Dhama <sdsunjay73@yahoo.com>
2018-03-11 16:48:10 -0700

    p16.py - Added more classifiers and params to classifiers already in use

--- nlp_with_python_and_nltk/p16.py
+++ nlp_with_python_and_nltk/p16.py
@@ -10,15 +10,34 @@
 import nltk
 import random
 from nltk.corpus import movie_reviews
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from nltk.classify.scikitlearn import SklearnClassifier
 import string
 
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.feature_extraction.text import HashingVectorizer
+# from sklearn.feature_selection import SelectFromModel
+# from sklearn.feature_selection import SelectKBest, chi2
+# from sklearn.pipeline import Pipeline
+
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestCentroid
 
 from nltk.classify import ClassifierI
 from statistics import mode, StatisticsError
+from sklearn import metrics
 
 # new code
 class VoteClassifier(ClassifierI):
@@ -78,37 +97,60 @@ def remove_punctuation_from_list(all_words):
     all_words = [s for s in all_words if s]
     return all_words
 
+def remove_stop_words_from_list(all_words):
+    stop_words = set(stopwords.words('english'))
+    # word_tokens = word_tokenize(example_sent)
+ 
+    filtered_sentence = [w for w in all_words if not w in stop_words]
+ 
+    filtered_sentence = []
+ 
+    for w in all_words:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+    return filtered_sentence
+
 
 def train_and_test_classifiers(train_set, test_set):
     classifier = nltk.NaiveBayesClassifier.train(train_set)
     print("Classic Naive Bayes Classifier accuracy percent:",
           (nltk.classify.accuracy(classifier, test_set)) * 100)
-    MNB_classifier = SklearnClassifier(MultinomialNB(alpha=0.01))
+    # classifier.show_most_informative_features(15)
+    MNB_classifier = SklearnClassifier(MultinomialNB(alpha=0.01, fit_prior=False))
     MNB_classifier.train(train_set)
     print("Multinomial Naive Bayes Classifier accuracy percent:",
           (nltk.classify.accuracy(MNB_classifier, test_set)) * 100)
 
     print("Skipping Gaussian Bayes Classifier accuracy percent")
-    GNB_classifier = SklearnClassifier(GaussianNB())
-    #GNB_classifier.fit(features_train, target_train)
+    # GNB_classifier = SklearnClassifier(GaussianNB())
+    # GNB_classifier.fit(features_train, target_train)
     # target_pred = clf.predict(features_test)
     # GNB_classifier.train(train_set)
     # print("Gaussian Naive Bayes Classifier accuracy percent:", (nltk.classify.accuracy(GNB_classifier, test_set))*100)
 
-    BNB_classifier = SklearnClassifier(BernoulliNB())
+    BNB_classifier = SklearnClassifier(BernoulliNB(alpha=.01))
     BNB_classifier.train(train_set)
     print("Bernoulli Naive Bayes Classifier accuracy percent:",
           (nltk.classify.accuracy(BNB_classifier, test_set)) * 100)
     
-    LG_classifier = SklearnClassifier(LogisticRegression(random_state=0))
+    LG_classifier = SklearnClassifier(LogisticRegression(random_state=42))
     LG_classifier.train(train_set)
     print("Logistic Regression Classifier accuracy percent:",
           (nltk.classify.accuracy(LG_classifier, test_set)) * 100)
+    
+    # Train SGD with hinge penalty
+    SGD_classifier1 = SklearnClassifier(SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3,
+                                                     random_state=42, max_iter=1000, tol=None))
+    # SGD_classifier = SklearnClassifier(SGDClassifier(alpha=0.0005, max_iter=1000))
+    SGD_classifier1.train(train_set)
+    print("Stochastic Gradient Descent Classifier 1 accuracy percent:",
+          (nltk.classify.accuracy(SGD_classifier1, test_set)) * 100)
 
-    SGD_classifier = SklearnClassifier(SGDClassifier(alpha=0.0005, max_iter=1000))
-    SGD_classifier.train(train_set)
-    print("Stochastic Gradient Descent Classifier accuracy percent:",
-          (nltk.classify.accuracy(SGD_classifier, test_set)) * 100)
+    # Train SGD with Elastic Net penalty
+    SGD_classifier2 = SklearnClassifier(SGDClassifier(alpha=1e-3, random_state=42, penalty="elasticnet", max_iter=1000, tol=None))
+    SGD_classifier2.train(train_set)
+    print("Stochastic Gradient Descent Classifier 2 accuracy percent:",
+          (nltk.classify.accuracy(SGD_classifier2, test_set)) * 100)
 
     # print("Skipping C-Support Vector Classifier")
     # print("Skipping Linear-Support Vector Classifier")
@@ -116,11 +158,18 @@ def train_and_test_classifiers(train_set, test_set):
     # SVC_classifier.train(train_set)
     print("C-Support Vector Classifier accuracy percent:",
           (nltk.classify.accuracy(SVC_classifier, test_set)) * 100)
-    LinearSVC_classifier = SklearnClassifier(SVC(kernel='linear', probability=True))
-    # LinearSVC_classifier = SklearnClassifier(LinearSVC())
-    LinearSVC_classifier.train(train_set)
-    print("Linear Support Vector Classifier accuracy percent:",
-          (nltk.classify.accuracy(LinearSVC_classifier, test_set)) * 100)
+    LinearSVC_classifier1 = SklearnClassifier(SVC(kernel='linear', probability=True, tol=1e-3))
+    LinearSVC_classifier1.train(train_set)
+    print("Linear Support Vector Classifier 1 accuracy percent:",
+          (nltk.classify.accuracy(LinearSVC_classifier1, test_set)) * 100)
+    LinearSVC_classifier2 = SklearnClassifier(LinearSVC("l1", dual=False, tol=1e-3))
+    LinearSVC_classifier2.train(train_set)
+    print("Linear Support Vector Classifier 2 accuracy percent:",
+          (nltk.classify.accuracy(LinearSVC_classifier2, test_set)) * 100)
+    LinearSVC_classifier3 = SklearnClassifier(LinearSVC("l2", dual=False, tol=1e-3))
+    LinearSVC_classifier3.train(train_set)
+    print("Linear Support Vector Classifier 3 accuracy percent:",
+          (nltk.classify.accuracy(LinearSVC_classifier3, test_set)) * 100)
 
     NuSVC_classifier = SklearnClassifier(NuSVC())
     NuSVC_classifier.train(train_set)
@@ -128,23 +177,50 @@ def train_and_test_classifiers(train_set, test_set):
           (nltk.classify.accuracy(NuSVC_classifier, test_set)) * 100)
 
     # new code
-    # voted_classifier = VoteClassifier(
-     #   classifier, MNB_classifier, BNB_classifier, LG_classifier, SGD_classifier, SVC_classifier,
-     #   LinearSVC_classifier, NuSVC_classifier)
-    #print("Voted Classifier Classifier accuracy percent:",
-    #      (nltk.classify.accuracy(voted_classifier, test_set)) * 100)
-    #print("Classification: ", voted_classifier.classify(test_set[0][
-    #      0]), "Confidence: %", voted_classifier.confidence(test_set[0][0]) * 100)
-    #print("Classification: ", voted_classifier.classify(test_set[2][
-    #      0]), "Confidence: %", voted_classifier.confidence(test_set[2][0]) * 100)
-    #print("Classification: ", voted_classifier.classify(test_set[3][
-    #      0]), "Confidence: %", voted_classifier.confidence(test_set[3][0]) * 100)
-    #print("Classification: ", voted_classifier.classify(test_set[4][
-    #      0]), "Confidence: %", voted_classifier.confidence(test_set[4][0]) * 100)
+    
+    # Train NearestCentroid (aka Rocchio classifier) without threshold
+    Nearest_Centroid_classifier = SklearnClassifier(NearestCentroid())
+    Nearest_Centroid_classifier.train(train_set)
+    print("Nearest Centroid Classifier accuracy percent:",
+          (nltk.classify.accuracy(Nearest_Centroid_classifier, test_set)) * 100)
+
+    Ridge_classifier = SklearnClassifier(RidgeClassifier(alpha=0.5, tol=1e-2, solver="sag"))
+    Ridge_classifier.train(train_set)
+    print("Ridge Classifier accuracy percent:",
+          (nltk.classify.accuracy(Ridge_classifier, test_set)) * 100)
+    
+    Perceptron_classifier = SklearnClassifier(Perceptron(max_iter=1000))
+    Perceptron_classifier.train(train_set)
+    print("Perceptron Classifier accuracy percent:",
+          (nltk.classify.accuracy(Perceptron_classifier, test_set)) * 100)
+    
+    Passive_Aggressive_classifier = SklearnClassifier(PassiveAggressiveClassifier(max_iter=1000))
+    Passive_Aggressive_classifier.train(train_set)
+    print("Passive-Aggressive Classifier accuracy percent:",
+          (nltk.classify.accuracy(Passive_Aggressive_classifier, test_set)) * 100)
+    
+    kNN_classifier = SklearnClassifier(KNeighborsClassifier(n_neighbors=10))
+    kNN_classifier.train(train_set)
+    print("kNN Classifier accuracy percent:",
+          (nltk.classify.accuracy(kNN_classifier, test_set)) * 100)
+    
+    voted_classifier = VoteClassifier(
+        classifier, MNB_classifier, BNB_classifier, LG_classifier, SGD_classifier2,
+    LinearSVC_classifier2, NuSVC_classifier)
+    print("Voted Classifier Classifier accuracy percent:",
+          (nltk.classify.accuracy(voted_classifier, test_set)) * 100)
+    print("Classification: ", voted_classifier.classify(test_set[0][
+        0]), "Confidence: %", voted_classifier.confidence(test_set[0][0]) * 100)
+    print("Classification: ", voted_classifier.classify(test_set[2][
+        0]), "Confidence: %", voted_classifier.confidence(test_set[2][0]) * 100)
+    print("Classification: ", voted_classifier.classify(test_set[3][
+        0]), "Confidence: %", voted_classifier.confidence(test_set[3][0]) * 100)
+    print("Classification: ", voted_classifier.classify(test_set[4][
+        0]), "Confidence: %", voted_classifier.confidence(test_set[4][0]) * 100)
+
+    # print(metrics.classification_report(twenty_test.target, predicted, target_names=twenty_test.target_names))
 
 # Main
-
-
 def main():
 
     # list of tuples
@@ -161,6 +237,7 @@ def main():
         all_words.append(w.lower())
 
     all_words = remove_punctuation_from_list(all_words)
+    all_words = remove_stop_words_from_list(all_words)
     all_words = nltk.FreqDist(all_words)
 
     featuresets = [(find_features(rev, all_words), category)
